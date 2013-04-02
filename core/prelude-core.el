1e375fb0f6716c49ed28fb892d21889371316cd3
Bozhidar Batsov <bozhidar@tradeo.com>
2013-04-02 12:14:10 +0300

    Update line moving commands

--- core/prelude-core.el
+++ core/prelude-core.el
@@ -110,39 +110,6 @@ Position the cursor at its beginning, according to the current mode."
   (forward-line -1)
   (indent-according-to-mode))
 
-(defun prelude-kill-whole-line (&optional arg)
-  "A simple wrapper around command `kill-whole-line' that respects indentation.
-Passes ARG to command `kill-whole-line' when provided."
-  (interactive "P")
-  (kill-whole-line arg)
-  (back-to-indentation))
-
-(defun prelude-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
-(global-set-key [remap move-beginning-of-line]
-                'prelude-move-beginning-of-line)
-
 (defun prelude-indent-buffer ()
   "Indent the currently visited buffer."
   (interactive)
